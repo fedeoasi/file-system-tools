646064fd5402bcf24926bbd9ab9a2cf48a7c8984
Federico Caimi <fedeoasi@gmail.com>
2018-01-13 14:42:44 -0600

    Find a folder by name

---
+++ src/main/scala/com/github/fedeoasi/FolderByName.scala
@@ -0,0 +1,15 @@
+package com.github.fedeoasi
+
+import com.github.fedeoasi.Model.DirectoryEntry
+
+object FolderByName {
+  /** Finds a folder by name (case insensitive)
+    *
+    * @param args [FOLDER]
+    */
+  def main(args: Array[String]): Unit = {
+    val entries = EntryPersistence.read(Constants.DefaultMetadataFile)
+    val matchedFolders = entries.collect { case d: DirectoryEntry if d.name.equalsIgnoreCase(args(0)) => d }
+    println(matchedFolders.mkString("\n"))
+  }
+}
