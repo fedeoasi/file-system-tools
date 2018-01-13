c08e88e35fa4d3163c93c25a355a3027a1654582
Federico Caimi <fedeoasi@gmail.com>
2018-01-13 14:38:26 -0600

    Forgot to commit the dependencies file

---
+++ project/Dependencies.scala
@@ -0,0 +1,9 @@
+import sbt._
+
+object Dependencies {
+  lazy val arm = "com.jsuereth" %% "scala-arm" % "2.0"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
+  lazy val commonsIo = "commons-io" % "commons-io" % "2.4"
+  lazy val commonsCodec = "commons-codec" % "commons-codec" % "1.10"
+  lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.5"
+}
