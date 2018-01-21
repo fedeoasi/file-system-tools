c488448615a41724845b38ea5afdb1f172d89373
Federico Caimi <fedeoasi@gmail.com>
2018-01-20 21:14:40 -0600

    Support custom metadata file

--- src/main/scala/com/github/fedeoasi/FilesBySize.scala
+++ src/main/scala/com/github/fedeoasi/FilesBySize.scala
@@ -5,7 +5,8 @@ import com.github.fedeoasi.Model.FileEntries
 object FilesBySize {
   /** Ranks files by size. */
   def main(args: Array[String]): Unit = {
-    val entries = EntryPersistence.read(Constants.DefaultMetadataFile)
+    val metadataFile = if (args.nonEmpty) args(0) else Constants.DefaultMetadataFile
+    val entries = EntryPersistence.read(metadataFile)
     val files = FileEntries(entries)
     val largestFiles = files.sortBy(_.size).reverse.take(50)
     println(largestFiles.mkString("\n"))
@@ -14,7 +15,8 @@ object FilesBySize {
 
 object TotalSize {
   def main(args: Array[String]): Unit = {
-    val entries = EntryPersistence.read(Constants.DefaultMetadataFile)
+    val metadataFile = if (args.nonEmpty) args(0) else Constants.DefaultMetadataFile
+    val entries = EntryPersistence.read(metadataFile)
     val totalSize = FileEntries(entries).map(_.size).sum
     println(s"The total size in bytes is $totalSize")
   }
