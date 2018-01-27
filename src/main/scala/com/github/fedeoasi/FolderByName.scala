b719d77b2dc642a33ed49db57dade23710227025
Federico Caimi <fedeoasi@gmail.com>
2018-01-27 16:48:09 -0600

    Proper command line app for FolderByName

--- src/main/scala/com/github/fedeoasi/FolderByName.scala
+++ src/main/scala/com/github/fedeoasi/FolderByName.scala
@@ -1,15 +1,40 @@
 package com.github.fedeoasi
 
-import com.github.fedeoasi.Model.DirectoryEntry
+import java.nio.file.{Path, Paths}
+
+import com.github.fedeoasi.Model.{DirectoryEntry, FileSystemEntry}
+import scopt.OptionParser
 
 object FolderByName {
-  /** Finds a folder by name (case insensitive)
-    *
-    * @param args [FOLDER]
-    */
+  case class FolderByNameConfig(folderName: Option[String] = None, catalog: Option[Path] = None)
+
+  def findFoldersByName(folderName: String, entries: Seq[FileSystemEntry]): Seq[DirectoryEntry] = {
+    entries.collect { case d: DirectoryEntry if d.name.equalsIgnoreCase(folderName) => d }
+  }
+
+  private val parser = new OptionParser[FolderByNameConfig](getClass.getSimpleName) {
+    head(getClass.getSimpleName)
+
+    opt[String]('n', "folderName").required()
+      .action { case (folderName, config) => config.copy(folderName = Some(folderName)) }
+      .text("The root folder from which to generate a catalog")
+
+    opt[String]('c', "catalog")
+      .action { case (catalog, config) => config.copy(catalog = Some(Paths.get(catalog))) }
+      .text("The catalog file (csv)")
+
+    help("help").text("prints this usage text")
+  }
+
+  /** Finds a folder by name (case insensitive) */
   def main(args: Array[String]): Unit = {
-    val entries = EntryPersistence.read(Constants.DefaultCatalogFilename)
-    val matchedFolders = entries.collect { case d: DirectoryEntry if d.name.equalsIgnoreCase(args(0)) => d }
-    println(matchedFolders.mkString("\n"))
+    parser.parse(args, FolderByNameConfig()) match {
+      case Some(FolderByNameConfig(Some(folderName), optionalCatalog)) =>
+        val catalog = optionalCatalog.getOrElse(Constants.DefaultCatalogPath)
+        val entries = EntryPersistence.read(catalog)
+        val matchedFolders = findFoldersByName(folderName, entries)
+        println(matchedFolders.mkString("\n"))
+      case _ =>
+    }
   }
 }
