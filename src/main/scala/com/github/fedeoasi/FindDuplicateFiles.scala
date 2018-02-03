88834fc5dc54fd29a3e559e22295a2ffe28acb69
Federico Caimi <fedeoasi@gmail.com>
2018-02-03 15:15:29 -0600

    Support extension parameter in FindDuplicateFiles
    * List duplicate files by size

--- src/main/scala/com/github/fedeoasi/FindDuplicateFiles.scala
+++ src/main/scala/com/github/fedeoasi/FindDuplicateFiles.scala
@@ -1,27 +1,29 @@
 package com.github.fedeoasi
 
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 
 import com.github.fedeoasi.Model.{FileEntries, FileEntry, FileSystemEntry}
-import com.github.fedeoasi.cli.CatalogConfig
 import scopt.OptionParser
 
 object FindDuplicateFiles {
+  case class FundDuplicateFilesConfig(
+    catalog: Option[Path] = None,
+    extension: Option[String] = None)
+
   //TODO Make this return a nice report case class instead of Unit
   def findDuplicates(entries: Seq[FileSystemEntry]): Unit = {
     val files = entries.collect { case f: FileEntry if f.md5.nonEmpty => f }
-    println(files.size)
+
     val duplicatesByMd5 = files.groupBy(_.md5).filter {
       case (_, entriesForMd5) =>
-        entriesForMd5.size > 1//&& entriesForMd5.size < 5
+        entriesForMd5.size > 1
     }
 
-    val duplicatesByMd5LessThanK = duplicatesByMd5.filter(_._2.size < 5)
-    val topK = duplicatesByMd5LessThanK.toSeq
-      .sortBy(_._2.size)
-      .reverse
-      .take(100)
-    println(topK.map(e => e._2.mkString("\n")).mkString("\n\n"))
+    val allDuplicates = duplicatesByMd5.mapValues(_.head).values.toSeq
+    val duplicatesBySize = allDuplicates.sortBy(_.size).reverse
+    val topK = duplicatesBySize.take(20)
+
+    println(topK.mkString("\n"))
 
     println()
 
@@ -30,26 +32,34 @@ object FindDuplicateFiles {
       case (_, filesForFolder) =>
         filesForFolder.size
     }
-    val foldersAndDuplicateCounts = duplicateCountByFolder.toSeq.sortBy(_._2).reverse.take(10)
+    val foldersAndDuplicateCounts = duplicateCountByFolder.toSeq.sortBy(_._2).reverse.take(20)
     println(foldersAndDuplicateCounts.mkString("\n"))
   }
 
-  private val parser = new OptionParser[CatalogConfig](getClass.getSimpleName) {
+  private val parser = new OptionParser[FundDuplicateFilesConfig](getClass.getSimpleName) {
     head(getClass.getSimpleName)
 
     opt[String]('c', "catalog")
       .action { case (catalog, config) => config.copy(catalog = Some(Paths.get(catalog))) }
       .text("The catalog file (csv)")
 
+    opt[String]('e', "extension")
+      .action { case (extension, config) => config.copy(extension = Some(extension)) }
+      .text("The extension of the files to be searched")
+
     help("help").text("prints this usage text")
   }
 
   def main(args: Array[String]): Unit = {
-    parser.parse(args, CatalogConfig()) match {
-      case Some(CatalogConfig(optionalCatalog)) =>
+    parser.parse(args, FundDuplicateFilesConfig()) match {
+      case Some(FundDuplicateFilesConfig(optionalCatalog, optionalExtension)) =>
         val catalog = optionalCatalog.getOrElse(Constants.DefaultCatalogPath)
         val files = FileEntries(EntryPersistence.read(catalog))
-        findDuplicates(files)
+        val filteredFiles = optionalExtension match {
+          case Some(extension) => files.filter(_.extension.exists(_.equalsIgnoreCase(extension)))
+          case None => files
+        }
+        findDuplicates(filteredFiles)
       case _ =>
     }
   }
