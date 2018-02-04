8d4d360ec2ad0fcba06979c9e977acffdea0829f
Federico Caimi <fedeoasi@gmail.com>
2018-02-04 13:51:37 -0600

    Support a hint folder in DeletionChecker so we only check for deletions in that folder

--- src/main/scala/com/github/fedeoasi/DeletionChecker.scala
+++ src/main/scala/com/github/fedeoasi/DeletionChecker.scala
@@ -1,25 +1,30 @@
 package com.github.fedeoasi
 
 import java.io.File
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 
 import com.github.fedeoasi.Model.FileSystemEntry
-import com.github.fedeoasi.cli.CatalogConfig
 import scopt.OptionParser
 
 object DeletionChecker {
+  case class DeletionCheckerConfig(catalog: Option[Path] = None, folder: Option[String] = None)
+
   //TODO make sure that the the parent folder exists, otherwise we will wipe out a catalog if you forgot to
   //connect your hard drive
   def findDeletions(entries: Seq[FileSystemEntry]): (Seq[FileSystemEntry], Seq[FileSystemEntry]) = {
     entries.partition(e => new File(e.path).exists())
   }
 
-  private val parser = new OptionParser[CatalogConfig](getClass.getSimpleName) {
+  private val parser = new OptionParser[DeletionCheckerConfig](getClass.getSimpleName) {
     head(getClass.getSimpleName)
 
     opt[String]('c', "catalog")
       .action { case (catalog, config) => config.copy(catalog = Some(Paths.get(catalog))) }
       .text("The catalog file (csv)")
+
+    opt[String]('f', "folder")
+      .action { case (folder, config) => config.copy(folder = Some(folder)) }
+      .text("The root from which to check for deletions")
 
     help("help").text("prints this usage text")
   }
@@ -27,13 +32,19 @@ object DeletionChecker {
   /** Performs deletions on the catalog
     */
   def main(args: Array[String]): Unit = {
-    parser.parse(args, CatalogConfig()) match {
-      case Some(CatalogConfig(optionalCatalog)) =>
+    parser.parse(args, DeletionCheckerConfig()) match {
+      case Some(DeletionCheckerConfig(optionalCatalog, optionalFolder)) =>
         val catalog = optionalCatalog.getOrElse(Constants.DefaultCatalogPath)
         val entries = EntryPersistence.read(catalog)
-        val (toKeep, toDelete) = findDeletions(entries)
+        val (toCheck, toKeepWithoutCheck) = optionalFolder match {
+          case Some(folder) =>
+            entries.partition(_.path.startsWith(folder))
+          case None =>
+            (entries, Seq.empty)
+        }
+        val (toKeep, toDelete) = findDeletions(toCheck)
         println(s"Found ${toDelete.size} entries to delete")
-        EntryPersistence.write(toKeep, catalog)
+        EntryPersistence.write(toKeepWithoutCheck ++ toKeep, catalog)
       case _ =>
     }
   }
