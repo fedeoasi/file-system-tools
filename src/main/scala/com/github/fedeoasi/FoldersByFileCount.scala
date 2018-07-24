2a562751bc26b40d99806559fbc751039acabadd
Federico Caimi <fedeoasi@gmail.com>
2018-07-24 07:58:30 -0500

    Command line arguments for FoldersByFileCount and use TopKFinder

--- src/main/scala/com/github/fedeoasi/FoldersByFileCount.scala
+++ src/main/scala/com/github/fedeoasi/FoldersByFileCount.scala
@@ -1,11 +1,30 @@
 package com.github.fedeoasi
 
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 
 import com.github.fedeoasi.Model._
+import scopt.OptionParser
+
+case class FoldersByFileConfig(catalog: Option[Path] = None, folder: Option[Path] = None)
 
 object FoldersByFileCount {
-  def fileCounts(root: String, entriesByParent: Map[String, Seq[FileSystemEntry]]): Map[String, Int] = {
+  private val parser = new OptionParser[FoldersByFileConfig](getClass.getSimpleName) {
+    head(getClass.getSimpleName)
+
+    opt[String]('c', "catalog")
+      .action { case (catalog, config) => config.copy(catalog = Some(Paths.get(catalog))) }
+      .text("The catalog file (csv)")
+      .required()
+
+    opt[String]('f', "folder")
+      .action { case (folder, config) => config.copy(folder = Some(Paths.get(folder))) }
+      .text("The root folder to analyze")
+      .required()
+
+    help("help").text("prints this usage text")
+  }
+
+  def fileCounts(root: Path, entriesByParent: Map[String, Seq[FileSystemEntry]]): Map[String, Int] = {
     //Gather directories so that they are sorted by level in reverse order, so that
     //we can rely on the file count for a child to be present when we compute the
     //file count for a folder
@@ -15,7 +34,7 @@ object FoldersByFileCount {
       folders.flatMap(d => gather(d.path, allFolders)) :+ folder
     }
 
-    val orderedFolders = gather(root, Seq.empty)
+    val orderedFolders = gather(root.toFile.getAbsolutePath, Seq.empty)
     orderedFolders.foldLeft(Map.empty[String, Int]) { case (countsByFolder, folder) =>
       val entries = entriesByParent.getOrElse(folder, Seq.empty)
       val folders = DirectoryEntries(entries)
@@ -37,14 +56,13 @@ object FoldersByFileCount {
     * @param args [CATALOG] [FOLDER]
     */
   def main(args: Array[String]): Unit = {
-    val catalog = args(0)
-    val folder = args(1)
-    val entries = EntryPersistence.read(catalog)
-
-    val entriesByParent = entries.groupBy(_.parent)
-
-    val countByFolder = fileCounts(folder, entriesByParent)
-
-    println(countByFolder.toSeq.sortBy(_._2).reverse.take(100).mkString("\n"))
+    parser.parse(args, FoldersByFileConfig()) match {
+      case Some(FoldersByFileConfig(Some(catalog), Some(folder))) =>
+        val entries = EntryPersistence.read(catalog)
+        val entriesByParent = entries.groupBy(_.parent)
+        val countByFolder = fileCounts(folder, entriesByParent)
+        println(new TopKFinder(countByFolder.toSeq).top(50)(Ordering.by(_._2)).mkString("\n"))
+      case _ =>
+    }
   }
 }
