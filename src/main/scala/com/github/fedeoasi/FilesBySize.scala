b0dbbfb8a5043981ca49da27fcdce8ad8458e560
Federico Caimi <fedeoasi@gmail.com>
2018-07-23 21:38:15 -0500

    Command line arguments for FilesBySize and TotalSize

--- src/main/scala/com/github/fedeoasi/FilesBySize.scala
+++ src/main/scala/com/github/fedeoasi/FilesBySize.scala
@@ -3,23 +3,30 @@ package com.github.fedeoasi
 import java.text.NumberFormat
 
 import com.github.fedeoasi.Model.FileEntries
+import com.github.fedeoasi.cli.{CatalogConfig, CatalogConfigParsing}
 
-object FilesBySize {
+object FilesBySize extends CatalogConfigParsing {
   /** Ranks files by size. */
   def main(args: Array[String]): Unit = {
-    val catalog = args(0)
-    val entries = EntryPersistence.read(catalog)
-    val files = FileEntries(entries)
-    val largestFiles = files.sortBy(_.size).reverse.take(50)
-    println(largestFiles.mkString("\n"))
+    parser.parse(args, CatalogConfig()) match {
+      case Some(CatalogConfig(Some(catalog))) =>
+        val entries = EntryPersistence.read(catalog)
+        val files = FileEntries(entries)
+        val largestFiles = files.sortBy(_.size).reverse.take(50)
+        println(largestFiles.mkString("\n"))
+      case _ =>
+    }
   }
 }
 
-object TotalSize {
+object TotalSize extends CatalogConfigParsing {
   def main(args: Array[String]): Unit = {
-    val catalog = args(0)
-    val entries = EntryPersistence.read(catalog)
-    val totalSize = FileEntries(entries).map(_.size).sum
-    println(s"The total size in bytes is ${NumberFormat.getIntegerInstance.format(totalSize)}")
+    parser.parse(args, CatalogConfig()) match {
+      case Some(CatalogConfig(Some(catalog))) =>
+        val entries = EntryPersistence.read(catalog)
+        val totalSize = FileEntries(entries).map(_.size).sum
+        println(s"The total size in bytes is ${NumberFormat.getIntegerInstance.format(totalSize)}")
+      case _ =>
+    }
   }
 }
